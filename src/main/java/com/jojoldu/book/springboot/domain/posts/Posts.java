e3d24aa32dc31dd69c84b035252f0b8308ffac5e
dhdydtn2019 <dhdydtn2019@gmail.com>
2019-12-22 21:47:53 +0900

    2019/12/22 4장 게시판 작성 /수정 /삭제 구현 완료

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,36 +1,43 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
-@NoArgsConstructor //기본 생성자 자동 추가
-@Entity //테이블과 링크될 클래스임을 나타냄 , 기본값으로 클래스 카멜케이스 이름을 언더스코어네이밍으로 테이블이름을 매칭
+@NoArgsConstructor
+@Entity
 public class Posts extends BaseTimeEntity {
-    @Id //해당 테이블의 PK 필드를 나타냅니다.
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙을 나타냄  GenerationType.IDENTITY
-    private Long id;                                     //은 outo_increment 옵션
 
-    @Column(length = 500 , nullable =false) //테이블 칼럼을 나타내며 선언하지 않아도 해당 클래스의 필드는 컬럼이됨
-    private String title;         // 변경이 필요한 옵션이 있으면 사용(varchar 기본값 255)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
     private String author;
 
-    @Builder //해당 클래스의 빌더 패턴 클래스를 생성 , 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
-    public Posts(String title , String content ,String author) {
+    @Builder
+    public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
     }
+
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
-
 }
